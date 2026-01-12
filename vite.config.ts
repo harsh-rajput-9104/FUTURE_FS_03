596ecf452e0c20ce04d1216c7b99aef5f2569db5
harsh-rajput-9104 <harshrajput9104@gmail.com>
2026-01-12 23:50:16 +0530

    making the website live

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/FUTURE_FS_03/",
   server: {
     host: "::",
     port: 8080,
